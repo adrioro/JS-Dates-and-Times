26bc42db238b9398d7b41eacaedf664920e0b12a
adrioro <artistsvillage@me.com>
2019-01-15 12:56:19 -0500

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 var date = new Date(); // Tue Jan 15 2019 11:53:39 GMT-0500 (Eastern Standard Time)"
+
+date.getTime()
+//1547574667514
+
+date.getDate()
+//15
+
+date.getMonth()
+//0
+
+date.getMinutes()
+//51
+
+date.getMilliseconds()
+//514
